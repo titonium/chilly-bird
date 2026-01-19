5aaa31dc3bfdf8151c4f5780c28f35a3e0f66de4
titonium <48673500+titonium@users.noreply.github.com>
2026-01-19 23:38:50 +0100

    apiKey

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDJwXWim06otLniUJn5X2IIvH4G0GfJm-Y",
+    authDomain: "chilly-bird.firebaseapp.com",
+    databaseURL: "https://chilly-bird-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chilly-bird",
+    storageBucket: "chilly-bird.firebasestorage.app",
+    messagingSenderId: "739217318151",
+    appId: "1:739217318151:web:cd53af3cbd60d1856965a2"
+};
